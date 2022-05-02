92d366c6b9422495c5806078ecb53a19a4413c25
AlessP93 <alespiscino@libero.it>
2022-05-02 23:35:28 +0200

    fine esecizio

--- js/main.js
+++ js/main.js
@@ -1,14 +1,39 @@
 // Scrivi un programma che stampi in console i numeri da 1 a 100
 const list = document.querySelector(".list")
-for (let i = 1; i <= 100; i++){
+for (let i = 1; i <= 100; i++) {
     // const element = `<li>${i}</li>`;
     // list.innerHTML = list.innerHTML + element;
     const element = document.createElement("li");
     element.classList.add("item");
     element.innerHTML = i;
     list.append(element);
+
+    // Per i multipli di entrambi stampi "FizzBuzz".
+    // if (i % 3) == 0 && (i % 5) == 0 {
+    //     element.classList.add("fizzbuzz");
+    //     console.log ("element");
+    // }
+    
+    // Per i multipli di 3 stampi “fizz” al posto del numero.
+    if  (i % 3 == 0 && i % 5 == 0) {
+        element.classList.add("fizzbuzz");
+    }
+
+    else if (i % 3 == 0) {
+        element.classList.add("fizz");
+    }
+
+    else if (i % 5 == 0) {
+        element.classList.add("buzz");
+    } 
+    
+    else {
+        element.innerHTML = i
+        console.log (i)
+    }
+    
+    
 }
 
-// Per i multipli di 3 stampi “Fizz” al posto del numero.
-// Per i multipli di 5 stampi "Buzz" al posto del numero.
-// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+
+
