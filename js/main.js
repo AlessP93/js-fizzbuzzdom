90f2b63a1aafadea4f3102991e9aaacbde06d96d
AlessP93 <alespiscino@libero.it>
2022-05-02 18:10:20 +0200

    commento js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-// Scrivi un programma che stampi in console i numeri da 1 a 100.
+// Scrivi un programma che stampi in console i numeri da 1 a 100
 const list = document.querySelector(".list")
 for (let i = 1; i <= 100; i++){
     // const element = `<li>${i}</li>`;
@@ -8,3 +8,7 @@ for (let i = 1; i <= 100; i++){
     element.innerHTML = i;
     list.append(element);
 }
+
+// Per i multipli di 3 stampi “Fizz” al posto del numero.
+// Per i multipli di 5 stampi "Buzz" al posto del numero.
+// Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
